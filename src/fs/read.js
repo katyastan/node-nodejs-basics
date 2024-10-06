9da854c543a2f94c2ea15acc9dc90fec03194f63
katyastan <134015338+katyastan@users.noreply.github.com>
2024-10-07 01:04:50 +0300

    task read - done

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import { join } from 'path';
+import { promises as fs } from 'fs';
+
 const read = async () => {
-    // Write your code here 
+    const filePath = join('src/fs/files', 'fileToRead.txt');
+    try {
+        await fs.access(filePath);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        }
+    }
+    const content = await fs.readFile(filePath, 'utf8');
+    console.log(content);
 };
 
 await read();
