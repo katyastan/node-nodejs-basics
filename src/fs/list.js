b02184e8a7123f60c73ceb2076d1ac64bfce619b
katyastan <134015338+katyastan@users.noreply.github.com>
2024-10-07 01:00:02 +0300

    task list - done

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+
 const list = async () => {
-    // Write your code here 
+    const filePath = join('src/fs/files');
+    try {
+        await fs.access(filePath);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        }
+    }
+    const files = await fs.readdir(filePath);
+    console.log(files);
 };
 
 await list();
