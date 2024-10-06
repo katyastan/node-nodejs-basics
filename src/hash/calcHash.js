4d4744bde506f7a5a9fd7750897655716396875f
katyastan <134015338+katyastan@users.noreply.github.com>
2024-10-07 02:52:59 +0300

    task calcHash - done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import { join } from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
-};
+    const filePath = join('src/hash/files', 'fileToCalculateHashFor.txt');
+    const fileStream = createReadStream(filePath);
+    const hash = createHash('sha256');
+    fileStream.pipe(hash);
+    await new Promise((resolve, reject) => {
+        fileStream.on('error', reject);
+        fileStream.on('end', resolve);
+    });
+    console.log(`Hash is ${hash.digest('hex')}`);};
 
 await calculateHash();
