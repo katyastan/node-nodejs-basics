d041a16bc5bf8312baf2d168142c61f83aac8496
katyastan <134015338+katyastan@users.noreply.github.com>
2024-10-07 01:37:48 +0300

    task env - done

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-    // Write your code here 
+    const env = process.env;
+    const prefix = 'RSS_';
+    const result = Object.keys(env).filter(key => key.startsWith(prefix)).map(key => `${key}=${env[key]}`)
+    console.log(result.join('; '));
 };
 
 parseEnv();
