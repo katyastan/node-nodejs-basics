765426fbf6e447d980438fff3ce62565cc9520bc
katyastan <134015338+katyastan@users.noreply.github.com>
2024-10-07 18:43:55 +0300

    task wt - done

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,29 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+  const numOfCores = cpus().length; 
+  const startNumber = 10;
+  const fbService = (n) =>
+    new Promise((resolve) => {
+      const worker = new Worker( './src/wt/worker.js', { workerData: n });
+      worker.postMessage(n);
+      worker.on('message', (data) => {
+        resolve({
+          status: 'resolved',
+          data: data,
+        });
+      });
+      worker.on('error', (error) => {
+        resolve({
+          status: 'error',
+          data: null,
+          error: error.message,
+        });
+      });
+    });
+  const workerResults = await Promise.all(Array.from({ length: numOfCores }, (_, i) => fbService(startNumber + i)));
+  console.log(workerResults);
 };
 
 await performCalculations();
